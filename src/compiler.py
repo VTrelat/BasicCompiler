e151c623bf2f9a028fa90d282d1c33a68134efd3
Vincent Trélat <vincent.trelat@free.fr>
2022-05-11 11:47:34 +0200

    Updates

--- src/compiler.py
+++ src/compiler.py
@@ -65,35 +65,80 @@ def prettify(program):
 
 
 # Build assembler code
-def body_asm(body):
+def expr_asm(expr):
+    asm = ""
+    if expr.data == "variable":
+        return f"    mov rax, [{expr.children[0].value}] \n"
+    
+    if expr.data == "number":
+        return f"    mov rax, {expr.children[0].value} \n"
+    
+    elif expr.data == "binexpr":
+        if expr.children[1].value == "+":
+            asm += expr_asm(expr.children[2])
+            asm += "    push rax\n"
+            asm += expr_asm(expr.children[0])
+            asm += "    add rax, rbx\n"
+        else :
+            raise Exception("Unknown operator")
+        return f"    {expr.children[1].value} rax, rbx"
+    
+    elif expr.data == "parenexpr":
+        expr_asm(expr.children[1])
+        return ""
+    
+    else:
+        raise Exception("Unknown expr")
+
+def cmd_asm(body):
     asm = ""
     for cmd in body.children:
         if cmd.data == "assignment":
-
+            asm += expr_asm(cmd.children[1])
+            asm += f"    mov [{cmd.children[0].value}], rax\n"
         elif cmd.data == "printf":
-
+            expr_asm(cmd.children[0])
+            asm += f"    call _printf\n"
         elif cmd.data == "while":
-
+            asm += f"    while_{cmd.children[0].value}: \n"
+            asm += expr_asm(cmd.children[1])
+            asm += f"    cmp rax, 0\n"
+            asm += f"    je endwhile_{cmd.children[0].value}\n"
+            asm += cmd_asm(cmd.children[2])
+            asm += f"    jmp while_{cmd.children[0].value}\n"
+            asm += f"endwhile_{cmd.children[0].value}: \n"
         elif cmd.data == "if":
+            asm += f"    if_{cmd.children[0].value}: \n"
+            asm += expr_asm(cmd.children[1])
+            asm += f"    cmp rax, 0\n"
+            asm += f"    je endif_{cmd.children[0].value}\n"
+            asm += cmd_asm(cmd.children[2])
+            asm += f"    jmp endif_{cmd.children[0].value}\n"
+            asm += f"endif_{cmd.children[0].value}: \n"
+    return asm
 
+def var_asm(variables):
+    return "\n".join([f"    {var.value}: dq 0" for var in variables.children])
 
 def build_assembler(program):
-    vars = prettify_variables(program.children[0])
-    body = prettify_bloc(program.children[1])
-    ret = prettify_expr(program.children[2])
+    vars = var_asm(program.children[0])
+    body = cmd_asm(program.children[1])
+    ret = expr_asm(program.children[2])
 
-    return f"section .data \n{vars} \nsection .text \nmain: \n{body} \nreturn: \n    mov eax, {ret} \n    ret"
+    return f"section .data \n{vars} \nsection .text \nmain: \n{body} \nreturn: \n{ret}    ret"
 
 
 program = grammar.parse(
     """
-main(x,y) {
+main(x, y) {
 x = 1;
 y = 2;
-return(x+y);
+return(x + y);
 }
 """)
+# print("Parsed program:")
+# print(prettify(program)+"\n")
 
-print(prettify(program))
 
+print("Assembler code:")
 print(build_assembler(program))
