51b389e741296027bbae2480ac6d99cf0d58454a
Bjiornulf <tpeev01@gmail.com>
2022-06-05 12:05:26 +0200

    taking into consideration if variable is an argument for the offsets, required to make function calls. using lists and dictionnaries instead of sets

--- src/utils.py
+++ src/utils.py
@@ -1,46 +1,52 @@
 from collections import namedtuple
 import lark
 
-
 # storage of function types and names
-Var = namedtuple("Var", ["type", "id"])
+Var = namedtuple("Var", ["type", "id", "argument",
+                 "pointer"], defaults=[False, False])
+Fun = namedtuple("Fun", ["type", "id", "tree"])
 
 
 def count_char(string, char):
     return sum(1 for c in string if c == char)
 
 
-def fun_list(prog: lark.Tree) -> set[Var]:
+def fun_list(prog: lark.Tree) -> dict[str, Fun]:
     return {
         # c.children[0].value == c.children[0]
-        Var(c.children[0], c.children[1])
+        c.children[1].value: Fun(c.children[0].value, c.children[1].value, c)
         for c in prog.children
     }
 
 
-def var_list(function: lark.Tree) -> set[Var]:
+def var_list(function: lark.Tree) -> list[Var]:
     if not isinstance(function, lark.Tree):
-        return set()
+        return []
     if function.data != "function":
-        return set()
+        return []
     # function.children[3] is the function bloc
     body = function.children[3]
     vars = function.children[2].children  # variables in function declaration
-    res = {Var(t.value.strip(), i.value.strip())
-           for t, i in zip(vars[::2], vars[1::2])}
+    res = [Var(t.value.strip(), i.value.strip(), True)
+           for t, i in zip(vars[::2], vars[1::2])]
     # adding the variables assigned in the bloc
-    res.update({
-        Var(c.children[0].value.strip(), c.children[1].value)
+    res += [
+        Var(c.children[0].value.strip(), c.children[1].value, False)
         for c in body.children
         if c.data == "initialization" or c.data == "declaration"
-    })
+    ]
     return res
 
 
-def var_offsets(vars: set[Var], types: dict[str, int]) -> dict[str, int]:
+def var_offsets(vars: list[Var], types: dict[str, int]) -> dict[str, int]:
     offset = 0
+    noffset = 8 + sum([types[v.type] for v in vars if v.argument])
     res = {}
     for v in vars:
-        offset += types[v.type]
-        res[v.id] = offset
+        if v.argument:
+            res[v.id] = noffset
+            noffset -= types[v.type]
+        else:
+            offset -= types[v.type]
+            res[v.id] = offset
     return res
